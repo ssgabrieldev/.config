8ae2ff4876a28aebd366d9740a4b621dc2273ab0
gabriel <ssgabrieldev@gmail.com>
2026-03-31 12:16:54 -0300

    asdf

--- nvim/lua/plugins/identblankline.lua
+++ nvim/lua/plugins/identblankline.lua
@@ -1,11 +1,11 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-        require("ibl").setup({
-            indent = {
-                char = "|"
-            }
-        })
-    end
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = function()
+    require("ibl").setup({
+      indent = {
+        char = "|"
+      }
+    })
+  end
 }
--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,49 +1,49 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = true,
+  opts = {
+    renderer = {
+      indent_markers = {
+        enable = true,
+        inline_arrows = true,
+        icons = {
+          corner = "|",
+          edge = "|",
+          item = "|",
+          bottom = " ",
+          none = " ",
+        },
+      },
     },
-    lazy = true,
-    opts = {
-        renderer = {
-            indent_markers = {
-                enable = true,
-                inline_arrows = true,
-                icons = {
-                    corner = "|",
-                    edge = "|",
-                    item = "|",
-                    bottom = " ",
-                    none = " ",
-                },
-            },
-        },
-        filters = {
-            enable = false
-        },
-        actions = {
-            open_file = {
-                resize_window = false,
-                window_picker = {
-                    enable = false
-                }
-            }
-        },
-        update_focused_file = {
-            enable = true
+    filters = {
+      enable = false
+    },
+    actions = {
+      open_file = {
+        resize_window = false,
+        window_picker = {
+          enable = false
         }
+      }
     },
-    keys = {
-        {
-            "<leader>ee",
-            function()
-                require("nvim-tree.api").tree.toggle()
-            end,
-            mode = {
-                "n"
-            },
-            desc = "Toggle NvimTree",
-            silent = true
-        }
-    },
+    update_focused_file = {
+      enable = true
+    }
+  },
+  keys = {
+    {
+      "<leader>ee",
+      function()
+        require("nvim-tree.api").tree.toggle()
+      end,
+      mode = {
+        "n"
+      },
+      desc = "Toggle NvimTree",
+      silent = true
+    }
+  },
 }
