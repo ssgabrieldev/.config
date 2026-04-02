83dc540d4cb6d4e9c07dc5ddc4d2dea7403ff537
gabriel <ssgabrieldev@gmail.com>
2026-04-02 17:59:09 -0300

    nvim: theme.lua

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'AlexvZyl/nordic.nvim',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nordic').load()
+    end
+  },
+  {
+    -- "ssgabrieldev/knot.nvim",
+    dir = "~/Documentos/my/dev/knot.nvim",
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      vim.cmd("color knot")
+    end,
+  }
+}
